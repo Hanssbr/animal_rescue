25cbf6ec79c3dad87accd79e403fad576d6fa909
Hanssbr <rayhaniqbalbr@gmail.com>
2024-12-06 16:06:37 +0700

    tidak menggunakan dashboaord nav blank

--- resources/views/pages/admins/report.blade.php
+++ resources/views/pages/admins/report.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.blank')
+@extends('layouts.adminboard')
 
 
 @section('page-heading', 'Daftar Hewan')
--- resources/views/pages/animals/index.blade.php
+++ resources/views/pages/animals/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.blank')
+@extends('layouts.dashboard')
 
 
 @section('page-heading', 'Daftar Hewan')
